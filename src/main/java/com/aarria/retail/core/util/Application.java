a662c001a85d74ece42d9800e18d92da4647f3cd
amirtharaj vellingiri <amirtharaj@live.ccom>
2021-04-11 22:01:05 +0530

    Images into jar

--- src/main/java/com/aarria/retail/core/util/Application.java
+++ src/main/java/com/aarria/retail/core/util/Application.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class Application {
 
-	public static final String IMAGES_DOMAIN = "http://localhost:80/resources/";
+	public static final String IMAGES_DOMAIN = "/resources/assets/images/resources/";
 
 	public static final String SEPARATOR = "/";
 
