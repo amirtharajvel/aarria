cad764dfb846145dab86e41258550b5c32344c44
amirtharaj vellingiri <amirtharaj@live.ccom>
2021-12-06 18:44:34 +0530

    Manual tomcat

--- src/main/java/com/aarria/retail/RetailApplication.java
+++ src/main/java/com/aarria/retail/RetailApplication.java
@@ -2,15 +2,23 @@ package com.aarria.retail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.aarria.retail"})
 @EnableJpaRepositories(basePackages = {"com.aarria.retail.persistence.repository", "com.aarria.retail.core.domain"})
-public class RetailApplication {
+public class RetailApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder
+                                                         application) {
+        return application.sources(RetailApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(RetailApplication.class, args);
     }
 
