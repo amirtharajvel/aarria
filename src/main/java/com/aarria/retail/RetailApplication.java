07c2d4639a14d63d50b0228e0e15b4808863514f
amirtharaj vellingiri <amirtharaj@live.ccom>
2022-04-13 16:57:56 +0530

    mysql timezone

--- src/main/java/com/aarria/retail/RetailApplication.java
+++ src/main/java/com/aarria/retail/RetailApplication.java
@@ -2,23 +2,15 @@ package com.aarria.retail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.aarria.retail"})
 @EnableJpaRepositories(basePackages = {"com.aarria.retail.persistence.repository", "com.aarria.retail.core.domain"})
-public class RetailApplication extends SpringBootServletInitializer {
+public class RetailApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder
-                                                         application) {
-        return application.sources(RetailApplication.class);
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(RetailApplication.class, args);
     }
 
