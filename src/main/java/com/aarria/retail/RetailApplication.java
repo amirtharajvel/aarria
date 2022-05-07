f12846171c20ec9159db90b5c941901b546972a3
amirtharaj vellingiri <amirtharaj@live.ccom>
2022-05-07 15:47:13 +0530

    jar to war

--- src/main/java/com/aarria/retail/RetailApplication.java
+++ src/main/java/com/aarria/retail/RetailApplication.java
@@ -2,13 +2,14 @@ package com.aarria.retail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.aarria.retail"})
 @EnableJpaRepositories(basePackages = {"com.aarria.retail.persistence.repository", "com.aarria.retail.core.domain"})
-public class RetailApplication {
+public class RetailApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RetailApplication.class, args);
